55c7f494a299e92f1c17d13f6430de31b9b656f6
stoff0 <147618447+stoff0@users.noreply.github.com>
2026-01-02 01:40:12 +0100

    Improved error handling

--- addons/PluginToggle/README.md
+++ addons/PluginToggle/README.md
@@ -4,4 +4,5 @@ A small Godot Plugin that adds a first-level toggle button to enable and disable
 ## How to use
 Simply add all plugins you want to control to the `plugins` Array in `plugintoggle.gd`.
 
+
 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/T6T21RCOVU)
--- addons/PluginToggle/plugintoggle.gd
+++ addons/PluginToggle/plugintoggle.gd
@@ -1,15 +1,35 @@
 @tool
 extends EditorPlugin
 
-# Replace "PluginToggle" with all plugin folders you want to be controlled:
-var plugins :PackedStringArray = ["PluginToggle"]
+# Replace "PluginToggle" with all plugin folders you want to control:
+const PLUGINS :Array = ["PluginToggle"]
+
+
 
 var toggle_button :Button
+var plugin_names :Array
 
 func _enter_tree():
+	var all = get_all_plugins()
+	if all.is_empty():
+		printerr("No Plugins found!")
+		return
+	elif PLUGINS.is_empty():
+		printerr("No Plugins specified!")
+		return
+	elif PLUGINS.size() == 1:
+		if PLUGINS[0] == "PluginToggle":
+			printerr("Please specify plugins to control within the " +
+					 "plugintoggle.gd and re-enable the plugin.")
+			return
+	for p in PLUGINS:
+		for a in all:
+			if a["folder"] == p:
+				plugin_names.append(a["name"])
 	toggle_button = CheckButton.new()
 	toggle_button.toggle_mode = true
-	toggle_button.tooltip_text = "Enables plugins:\n" + "\n".join(plugins)
+	toggle_button.text = "Plugins"
+	toggle_button.tooltip_text = "Controls Plugins:\n" + "\n".join(plugin_names)
 	toggle_button.toggled.connect(_on_toggle_toggled)
 	add_control_to_container(CONTAINER_TOOLBAR, toggle_button)
 
@@ -21,5 +41,30 @@ func _exit_tree():
 
 
 func _on_toggle_toggled(button_pressed: bool) -> void:
-	for p in plugins:
+	for p in PLUGINS:
 		EditorInterface.set_plugin_enabled(p, button_pressed)
+	if button_pressed:
+		prints("Plugin Enabled:", plugin_names)
+	else:
+		prints("Plugin Disabled:", plugin_names)
+
+func get_all_plugins() -> Array:
+	var result := []
+	var d := DirAccess.open("res://addons")
+	if d == null:
+		return result
+	d.list_dir_begin()
+	var name := d.get_next()
+	while name != "":
+		if d.current_is_dir() and not name.begins_with("."):
+			var cfg_path := "res://addons/"+name+"/plugin.cfg"
+			if FileAccess.file_exists(cfg_path):
+				var cfg := ConfigFile.new()
+				if cfg.load(cfg_path) == OK:
+					result.append({
+						"folder": name,
+						"name": cfg.get_value("plugin", "name", name),
+					})
+		name = d.get_next()
+	d.list_dir_end()
+	return result
